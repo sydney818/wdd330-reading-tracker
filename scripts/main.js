f73e19638bff4cdf951e9e9e6dc22f4b3617fb5f
sydney818 <sydney818@gmail.com>
2026-02-02 00:10:25 -0700

    Initial homepage styling and layout

---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+// Footer - Current Year & Last Modified Date
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+document.getElementById("lastModified").textContent = "Last Modified:" + document.lastModified;
